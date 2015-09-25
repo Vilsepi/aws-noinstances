342b5adbd98056304496af6dd86c4e58a0f2f8d5
Ville Seppanen <ville.seppanen@gofore.com>
2015-09-25 23:05:47 +0300

    Added instructions to update api script when given wrong params

--- update-api.sh
+++ update-api.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ansible-playbook --extra-vars "importer_mode=update interface_name=$1 interface_id=$2 interface_stage=$3" -i localhost, deploy.yml
+if [ "$#" -eq 3 ]; then
+    ansible-playbook --extra-vars "importer_mode=update interface_name=$1 interface_id=$2 interface_stage=$3" -i localhost, deploy.yml
+else
+	echo "Usage: ./update-api.sh interface_name interface_id interface_stage"
+	echo "Usage: ./update-api.sh main vz5y30yrel prod"
+fi
